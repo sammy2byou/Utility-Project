5e9689dccec107928cd7c21f95723e390aba28f9
Samantha McKenzie <80702255+sammy2byou@users.noreply.github.com>
2023-03-01 16:58:48 -0500

    Fixed sql

--- creatingTables.sql
+++ creatingTables.sql
@@ -1,5 +1,6 @@
+--For milestone 2
 CREATE TABLE UTILITY_PROVIDER
-( late_fee            FLOAT,
+( late_fee            DECIMAL,
   business_num        INT         NOT NULL,
   address             VARCHAR(),
   service_region      VARCHAR(), --Multi-value attribute, how show that in sql?
