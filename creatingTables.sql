ec7a4ff09bf3d97802b810156b2de99227f20ba1
Samantha McKenzie <80702255+sammy2byou@users.noreply.github.com>
2023-03-01 16:57:30 -0500

    Create file
    Created file for the sql create tables
    Temp added UTILITY_PROVIDER table

---
+++ creatingTables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE UTILITY_PROVIDER
+( late_fee            FLOAT,
+  business_num        INT         NOT NULL,
+  address             VARCHAR(),
+  service_region      VARCHAR(), --Multi-value attribute, how show that in sql?
+  utilities_offered   VARCHAR(),
+  email               VARCHAR(),
+  phone               VARCHAR(),
+PRIMARY KEY   (business_num));
