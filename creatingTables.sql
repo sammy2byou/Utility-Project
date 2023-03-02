15742b52d5513ff18e3fd827d21b95d668e243af
sammy2byou <sammy2bpizza@gmail.com>
2023-03-02 13:54:02 -0500

    Added temp values
    Fixing SQL syntax so the sql will execute

--- creatingTables.sql
+++ creatingTables.sql
@@ -2,9 +2,9 @@
 CREATE TABLE UTILITY_PROVIDER
 ( late_fee            DECIMAL,
   business_num        INT         NOT NULL,
-  address             VARCHAR(),
-  service_region      VARCHAR(), --Multi-value attribute, how show that in sql?
-  utilities_offered   VARCHAR(),
-  email               VARCHAR(),
-  phone               VARCHAR(),
-PRIMARY KEY   (business_num));
+  address             VARCHAR(5),
+  service_region      VARCHAR(5), --Multi-value attribute, how show that in sql?
+  utilities_offered   VARCHAR(5),
+  email               VARCHAR(5),
+  phone               VARCHAR(5),
+PRIMARY KEY	(business_num));
