ec270941cce831f54d52b7cb1c7a62aee7f08bf5
Josiah Henson <pbinspanish@outlook.com>
2023-04-06 12:06:11 -0400

    more views, incomplete

---
+++ views/ACCOUNT_INVOICES.sql
@@ -0,0 +1,33 @@
+-- DROP VIEW ACCOUNT_INVOICES;
+
+CREATE VIEW ACCOUNT_INVOICES AS
+SELECT DISTINCT
+	INVOICE.account_number AS "Account",
+	INVOICE.invoice_number AS "Invoice",
+	INVOICE.issue_date AS "Issue Date",
+	INVOICE.due_date AS "Invoice Due",
+	-- Subtotal
+	-- For each invoice
+	-- Sum of each LINE_ITEM.cost * INVOICE_ITEM.quantity
+	(
+		SELECT SUM(Sum) FROM (
+			SELECT
+				INVOICE.invoice_number,
+				INVOICE_ITEMS.Sum
+			FROM
+				INVOICE,
+				INVOICE_ITEMS
+			WHERE 
+				INVOICE.invoice_number = INVOICE_ITEMS.Invoice
+		) AS invoice_sums WHERE invoice_sums.invoice_number = INVOICE.invoice_number
+	) AS "Subtotal",
+	-- Tax
+	-- Tax is REGION.tax_rate * subtotal
+	-- This requires pulling REGION from CUSTOMER_ADDRESS from CUSTOMER from ACCOUNT from INVOICE
+	REGION.tax_rate AS "Tax Rate"
+FROM INVOICE, INVOICE_ITEMS, ACCOUNT
+JOIN CUSTOMER ON ACCOUNT.customer_id = CUSTOMER.customer_id
+JOIN CUSTOMER_ADDRESS ON CUSTOMER.customer_id = CUSTOMER_ADDRESS.customer_id
+JOIN REGION ON CUSTOMER_ADDRESS.region = REGION.region_id
+WHERE INVOICE_ITEMS.Invoice = INVOICE.invoice_number AND INVOICE.account_number = ACCOUNT.account_number
+GROUP BY INVOICE.account_number, INVOICE.invoice_number, INVOICE.issue_date, INVOICE.due_date, REGION.tax_rate;
---
+++ views/invoice_items.sql
@@ -0,0 +1,13 @@
+-- DROP VIEW INVOICE_ITEMS;
+
+CREATE VIEW INVOICE_ITEMS AS
+SELECT
+	INVOICE.account_number AS "Account Number",
+	INVOICE.invoice_number AS "Invoice",
+	INVOICE_ITEM.line_item AS "Line Item",
+	INVOICE_ITEM.quantity AS "Quantity",
+	LINE_ITEM.cost AS "Cost", -- this needs to also handle the utility case
+	INVOICE_ITEM.quantity * LINE_ITEM.cost AS "Sum"
+FROM INVOICE
+JOIN INVOICE_ITEM ON INVOICE.invoice_number = INVOICE_ITEM.invoice_number
+JOIN LINE_ITEM ON LINE_ITEM.line_item = INVOICE_ITEM.line_item
